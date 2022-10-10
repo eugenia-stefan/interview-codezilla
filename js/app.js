5c0e00009a9869b6eebef940e3468bbb6aafe7b8
eugenia-stefan <stefaneugeniagabriela@gmail.com>
2022-10-11 00:23:52 +0300

    add question 3

--- js/app.js
+++ js/app.js
@@ -1,23 +1,61 @@
 (function ($) {
+  "use strict";
 
-    'use strict';
+  // STICKY
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
 
-	// STICKY
-	$(window).scroll(function() {
-	    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $(".sticky").addClass("nav-sticky");
+    } else {
+      $(".sticky").removeClass("nav-sticky");
+    }
+  });
 
-	    if (scroll >= 50) {
-	        $(".sticky").addClass("nav-sticky");
-	    } else {
-	        $(".sticky").removeClass("nav-sticky");
-	    }
-	});
+  // scrollspy
+  $("#mySidenav").scrollspy({
+    offset: 70,
+  });
 
-	// scrollspy
-	$("#mySidenav").scrollspy({
-	    offset: 70
-	});
+  /* Add your code after this line */
+})(jQuery);
 
-	/* Add your code after this line */
+function onSubmitContactForm() {
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+  const comments = document.getElementById("comments").value;
 
-})(jQuery)
+  if (
+    hasValue(name) &&
+    hasValue(comments) &&
+    isValidEmail(email) &&
+    isValidPhoneNumber(phone)
+  ) {
+    document.getElementById("success-message").style.display = "inline-block";
+    document.getElementById("error-message").style.display = "none";
+  } else {
+    document.getElementById("success-message").style.display = "none";
+    document.getElementById("error-message").style.display = "inline-block";
+  }
+}
+
+function hasValue(value) {
+  if (!value) return false;
+  return true;
+}
+
+function isValidEmail(value) {
+  if (!hasValue(value)) return false;
+
+  const filter =
+    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  return filter.test(value);
+}
+
+function isValidPhoneNumber(value) {
+  if (!hasValue(value)) return false;
+
+  return /^\d+$/.test(value);
+}
